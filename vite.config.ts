fece744bec46e2bf48d46a43379b5f1bff0ab9ed
alexsmagalhaes <alexmagalhaesprofissional@gmail.com>
2024-03-02 16:23:49 -0300

    Add(Image compression): images
    Add a vite config to compress images in built.

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,78 @@
-import { defineConfig } from 'vite'
+import { build, defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import path from 'path'
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    ViteImageOptimizer(
+      {
+        test: /\.(jpe?g|png|gif|tiff|webp|svg|avif)$/i,
+        exclude: undefined,
+        include: undefined,
+        includePublic: true,
+        logStats: true,
+        ansiColors: true,
+        svg: {
+          multipass: true,
+          plugins: [
+            {
+              name: 'preset-default',
+              params: {
+                overrides: {
+                  cleanupNumericValues: false,
+                  removeViewBox: false, // https://github.com/svg/svgo/issues/1128
+                },
+              },
+            },
+            'sortAttrs',
+            {
+              name: 'addAttributesToSVGElement',
+              params: {
+                attributes: [{ xmlns: 'http://www.w3.org/2000/svg' }],
+              },
+            },
+          ],
+        },
+        png: {
+          // https://sharp.pixelplumbing.com/api-output#png
+          quality: 90,
+        },
+        jpeg: {
+          // https://sharp.pixelplumbing.com/api-output#jpeg
+          quality: 90,
+        },
+        jpg: {
+          // https://sharp.pixelplumbing.com/api-output#jpeg
+          quality: 90,
+        },
+        tiff: {
+          // https://sharp.pixelplumbing.com/api-output#tiff
+          quality: 90,
+        },
+        // gif does not support lossless compression
+        // https://sharp.pixelplumbing.com/api-output#gif
+        gif: {},
+        webp: {
+          // https://sharp.pixelplumbing.com/api-output#webp
+          lossless: true,
+        },
+        avif: {
+          // https://sharp.pixelplumbing.com/api-output#avif
+          lossless: true,
+        },
+        cache: true,
+        cacheLocation: undefined,
+      }
+    ),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src")
     }
   }
 })
+
+
