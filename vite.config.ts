27f4e8adbbc59c4fb7685edcf0968b9823f49d28
alexsmagalhaes <alexmagalhaesprofissional@gmail.com>
2024-03-09 10:54:11 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,63 +1,72 @@
-import { build, defineConfig } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import path from 'path'
 import { ViteImageOptimizer } from 'vite-plugin-image-optimizer'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    // ViteImageOptimizer(
-    //   {
-    //     test: '/\.(jpe?g|png|gif|tiff|webp|svg|avif)$/i',
-    //     includePublic: true,
-    //     logStats: true,
-    //     ansiColors: true,
-    //     svg: {
-    //       multipass: true,
-    //       plugins: [
-    //         {
-    //           name: 'preset-default',
-    //           params: {
-    //             overrides: {
-    //               cleanupNumericValues: false,
-    //               removeViewBox: false, // https://github.com/svg/svgo/issues/1128
-    //             },
-
-    //           },
-    //         },
-    //       ],
-    //     },
-    //     png: {
-    //       // https://sharp.pixelplumbing.com/api-output#png
-    //       quality: 100,
-    //     },
-    //     jpeg: {
-    //       // https://sharp.pixelplumbing.com/api-output#jpeg
-    //       quality: 100,
-    //     },
-    //     jpg: {
-    //       // https://sharp.pixelplumbing.com/api-output#jpeg
-    //       quality: 100,
-    //     },
-    //     tiff: {
-    //       // https://sharp.pixelplumbing.com/api-output#tiff
-    //       quality: 100,
-    //     },
-    //     // gif does not support lossless compression
-    //     // https://sharp.pixelplumbing.com/api-output#gif
-    //     gif: {},
-    //     webp: {
-    //       // https://sharp.pixelplumbing.com/api-output#webp
-    //       lossless: true,
-    //     },
-    //     avif: {
-    //       // https://sharp.pixelplumbing.com/api-output#avif
-    //       lossless: true,
-    //     },
-    //     cache: true,
-    //   }
-    // ),
+    ViteImageOptimizer({
+      test: /\.(jpe?g|png|gif|tiff|webp|svg|avif)$/i,
+      exclude: undefined,
+      include: undefined,
+      includePublic: true,
+      logStats: true,
+      ansiColors: true,
+      svg: {
+        multipass: true,
+        plugins: [
+          {
+            name: 'preset-default',
+            params: {
+              overrides: {
+                cleanupNumericValues: false,
+                removeViewBox: false, // https://github.com/svg/svgo/issues/1128
+              },
+              floatPrecision: 2, // Example float precision, adjust as needed
+            },
+          },
+          'sortAttrs',
+          {
+            name: 'addAttributesToSVGElement',
+            params: {
+              attributes: [{ xmlns: 'http://www.w3.org/2000/svg' }],
+            },
+          },
+        ],
+      },
+      png: {
+        // https://sharp.pixelplumbing.com/api-output#png
+        quality: 90,
+      },
+      jpeg: {
+        // https://sharp.pixelplumbing.com/api-output#jpeg
+        quality: 90,
+      },
+      jpg: {
+        // https://sharp.pixelplumbing.com/api-output#jpeg
+        quality: 90,
+      },
+      tiff: {
+        // https://sharp.pixelplumbing.com/api-output#tiff
+        quality: 90,
+      },
+      // gif does not support lossless compression
+      // https://sharp.pixelplumbing.com/api-output#gif
+      gif: {},
+      webp: {
+        // https://sharp.pixelplumbing.com/api-output#webp
+        lossless: true,
+      },
+      avif: {
+        // https://sharp.pixelplumbing.com/api-output#avif
+        lossless: true,
+      },
+      cache: true,
+      cacheLocation: undefined,
+    })
   ],
   resolve: {
     alias: {
