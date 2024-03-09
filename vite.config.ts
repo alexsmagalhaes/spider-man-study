1657852266a1a7536f5c2cef7b16ebcf513c50aa
alexsmagalhaes <alexmagalhaesprofissional@gmail.com>
2024-03-09 11:35:43 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 import path from 'path'
 import { ViteImageOptimizer } from 'vite-plugin-image-optimizer'
 
-const qualityRatio: number = 90;
+const qualityRatio: number = 80;
 
 // https://vitejs.dev/config/
 export default defineConfig({
