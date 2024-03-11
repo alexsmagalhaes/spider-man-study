8925526967559bca410bc8313ffc46b0b6974f2f
alexsmagalhaes <alexmagalhaesprofissional@gmail.com>
2024-03-11 15:23:27 -0300

    feat(add vercel analytics and insight)

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { GlobalStyles } from "./GlobalStyled"
 import { Outlet } from "react-router-dom"
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/react"
+import { Analytics } from '@vercel/analytics/react';
 
 import Navbar from "./layouts/Navbar"
 import Footer from "./layouts/Footer"
@@ -10,9 +11,10 @@ function App() {
     <>
       <GlobalStyles />
       <Navbar />
-        <Outlet />
+      <Outlet />
       <Footer />
-      <SpeedInsights/>
+      <SpeedInsights />
+      <Analytics />
     </>
   )
 }
